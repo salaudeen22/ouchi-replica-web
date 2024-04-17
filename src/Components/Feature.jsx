ed5d13b12e34ddd533c308d83d39a8efac8fa2b9
salaudeen22 <salaudeensalu@gmail.com>
2024-04-17 15:12:03 +0530

    done with animation working on responsive

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,10 +1,19 @@
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 
 function Feature() {
-  const handlehover=()=>
-  {
-    
-  }
+  const cards = [useAnimation(), useAnimation()];
+
+  const handleHover = (index) => {
+    cards[index].start({
+      y: "0%",
+    });
+  };
+  const handleHoverEnd = (index) => {
+    cards[index].start({
+      y: "100%",
+    });
+  };
+
   return (
     <div className="w-full py-10">
       <div className="w-full px-10 border-b-[1px] border-zinc-700 pb-20">
@@ -12,13 +21,25 @@ function Feature() {
       </div>
       <div className="px-20">
         <div className="flex gap-10 cards w-full mt-10">
-          <div onMouseEnter={()=>
-          {
-            handlehover();
-          }} className="cardcontainer relative w-1/2 h-[75vh]">
-            <h1 className="absolute left-full -translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA67] font-['Founders Grotesk X-Condensed'] font-semibold z-[9] tracking-tighter leading-none text-8xl">
+          <motion.div
+            onHoverStart={() => {
+              handleHover(0);
+            }}
+            onHoverEnd={() => {
+              handleHoverEnd(0);
+            }}
+            className="relative cardcontainer  w-1/2 h-[75vh]"
+          >
+            <h1 className="absolute flex right-0 overflow-hidden translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA67] font-['Founders Grotesk X-Condensed'] font-semibold z-[9] tracking-tighter leading-none text-8xl">
               {"FYDE".split("").map((item, index) => (
-                <motion.span key={index}>{item}</motion.span>
+                <motion.span
+                  initial={{ y: "100%" }}
+                  transition={{ ease: [0.22, 1, 0.36, 1], delay: index * 0.05 }}
+                  animate={cards[0]}
+                  key={index}
+                >
+                  {item}
+                </motion.span>
               ))}
             </h1>
             <div className="card w-full h-full rounded-xl bg-green-400 overflow-hidden">
@@ -28,13 +49,22 @@ function Feature() {
                 alt=""
               />
             </div>
-          </div>
-          <div className="relative cardcontainer w-1/2 h-[75vh]">
+          </motion.div>
+          <motion.div
+            onHoverStart={() => {
+              handleHover(1);
+            }}
+            onHoverEnd={() => {
+              handleHoverEnd(1);
+            }}
+            className="relative cardcontainer w-1/2 h-[75vh]"
+          >
             <h1 className="absolute flex right-full overflow-hidden translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA67] font-['Founders Grotesk X-Condensed'] font-semibold z-[9] tracking-tighter leading-none text-8xl">
               {"VIDE".split("").map((item, index) => (
                 <motion.span
-                  className="inline-block translate-y-full"
                   initial={{ y: "100%" }}
+                  transition={{ ease: [0.22, 1, 0.36, 1], delay: index * 0.05 }}
+                  animate={cards[1]}
                   key={index}
                 >
                   {item}
@@ -48,7 +78,7 @@ function Feature() {
                 alt=""
               />
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
